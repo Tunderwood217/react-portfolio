4a6cf86fadaf652836ff743edc978a3f687036ef
Tiffani Underwood <tdunderwood217@gmail.com>
2024-01-20 15:16:22 -0500

    added initial styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,24 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
     return (
-        <div>
-            <Link to="/">About Me</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/portfolio">Portfolio</Link>
-            <Link to="/resume">Resume</Link>
-            </div>
+    
+        <nav>
+            <h1>Tiffani Underwood</h1>
+            <ul className="navlink">
+                <li>
+            <Link to="/" className="navigation">About Me</Link>
+                </li>
+                <li>   
+            <Link to="/contact" className="navigation">Contact</Link>
+                </li>
+                <li>
+            <Link to="/portfolio" className="navigation">Portfolio</Link>
+                </li>
+                <li>
+            <Link to="/resume" className="navigation">Resume</Link>
+                </li>
+            </ul>
+            </nav>
     )
 }
 
